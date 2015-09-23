eb327e75b0ecf903885b3a38ca0addcc7314a9d5
Archie Fielding <archiefielding@gmail.com>
2015-09-23 15:50:49 +0100

    added heroku database

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,10 +5,10 @@ require './app/models/tag'
 env = ENV['RACK_ENV'] || 'development'
 
 if ENV['RACK_ENV'] == 'production'
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-else
-  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-end
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
+# else
+#   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+# end
 
 DataMapper.finalize
 
